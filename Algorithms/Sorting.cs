9717c3dba4e5a54c66853c7c72344b40deeb8a1e
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2020-07-30 07:44:25 -0400

    Add project files.

---
+++ Algorithms/Sorting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    public class Sorting
+    {
+        public void BubbleSort(int[] array)
+        {
+            // Outer loop - wall - 
+        }
+    }
+}
