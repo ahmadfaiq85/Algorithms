9ff7494748583d7216e39b98c5b22d89455ff589
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2020-08-01 10:58:56 -0400

    Node

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -7,6 +7,30 @@ namespace Algorithms
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            CreateNodes();
+        }
+
+        public static void CreateNodes()
+        {
+            Node first = new Node() { Value = 5 };
+            Node second = new Node() { Value = 1 };
+
+            first.Next = second;
+
+            Node third = new Node() { Value = 3 };
+            second.Next = third;
+
+            PrintLinkedList(first);
+        }
+
+        private static void PrintLinkedList(Node node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
         }
     }
 }
