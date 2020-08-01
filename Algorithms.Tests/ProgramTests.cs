25b3927ede9f0599ae80c5b938fa86fed3acde6d
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2020-07-31 21:31:46 -0400

    Factorial Recursive

---
+++ Algorithms.Tests/ProgramTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+
+namespace Algorithms.Tests
+{
+    class ProgramTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Factorial_Test()
+        {
+            int num = 4;
+            var factorial = new Algorithms.Factorial();
+
+            Assert.AreEqual(24, factorial.RecursiveFactorial(num));
+
+        }
+
+    }
+}
---
+++ Algorithms/Factorial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    public class Factorial
+    {
+        //n! = n * (n-1)!
+        public int RecursiveFactorial(int n)
+        {
+            if (n == 0)
+                return 1;
+
+            return n * RecursiveFactorial(n - 1);
+        }
+    }
+}
